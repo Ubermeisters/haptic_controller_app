77b8eeebc8789b6e6dc11a64a5bcd549d0df5341
Andrew Hepp <drewhepp@umich.edu>
2020-10-25 08:04:37 -0400

    added basic haptics menu

--- haptics.ino
+++ haptics.ino
@@ -17,8 +17,11 @@ std::vector<Tickable*> components;
 
 ButtonContext* menu_ctx = nullptr;
 ButtonContext* led_ctx = nullptr;
+ButtonContext* haptic_ctx = nullptr;
 
-Adafruit_DRV2605* drv;
+constexpr int MAX_EFFECT = 123;
+Adafruit_DRV2605 drv;
+int effect = 0;
 
 void setup() {
   Serial.begin(BAUD_RATE);
@@ -26,20 +29,23 @@ void setup() {
   
   // Assign modes for all pins
   pinMode(PIN_LED_0, OUTPUT);
-  pinMode(PIN_BUTTON_0, INPUT_PULLUP);
+  pinMode(PIN_BUTTON_0, INPUT_PULLUP); // Active low, yours may be different
 
   // Turn off LED_0
   digitalWrite(PIN_LED_0, LOW);
 
   // Initialize DRV
-  drv = new Adafruit_DRV2605();
+  drv.begin();
+  drv.selectLibrary(1);
+  drv.setMode(DRV2605_MODE_INTTRIG);
 
   // Allocate aux button and push into list of components
-  aux_button = new DebouncedButton(PIN_BUTTON_0, false);
+  aux_button = new DebouncedButton(PIN_BUTTON_0, false); // Again, active low
   components.push_back(aux_button);
 
-  define_menu_ctx();
-  define_led_ctx();
+  init_menu_ctx();
+  init_led_ctx();
+  init_haptic_ctx();
 
   aux_button->set_context(menu_ctx);
   
@@ -53,33 +59,35 @@ void loop() {
   }
 }
 
-void define_menu_ctx() {
-  Serial.println("defining menu_ctx");
+void print_duration(unsigned long duration) {
+  Serial.print("released after ");
+  Serial.print(duration); 
+  Serial.println("ms");
+}
+
+void init_menu_ctx() {
+  Serial.println("initializing menu_ctx");
   menu_ctx = new ButtonContext();
   menu_ctx->on_press([&]() {
     Serial.println("pressed");
   });
   menu_ctx->on_release([&](unsigned long duration) {
-    Serial.print("released after ");
-    Serial.print(duration);
-    Serial.println("ms");
+    print_duration(duration);
     if(duration > LONG_PRESS_MS) {
       menu_ctx = aux_button->release_context();
-      aux_button->set_context(led_ctx);
+      aux_button->set_context(haptic_ctx);
     }
   });
 }
 
-void define_led_ctx() {
-  Serial.println("defining led_ctx");
+void init_led_ctx() {
+  Serial.println("initializing led_ctx");
   led_ctx = new ButtonContext();
   led_ctx->on_press([&]() {
     Serial.println("pressed");
   });
   led_ctx->on_release([&](unsigned long duration) {
-    Serial.print("released after ");
-    Serial.print(duration);
-    Serial.println("ms");
+    print_duration(duration);
     
     if(duration < SHORT_PRESS_MS) {
       digitalWrite(PIN_LED_0, HIGH);
@@ -89,5 +97,32 @@ void define_led_ctx() {
       led_ctx = aux_button->release_context();
       aux_button->set_context(menu_ctx);
     }
+  });
+}
+
+void init_haptic_ctx() {
+  Serial.println("initializing haptic_ctx");
+  haptic_ctx = new ButtonContext();
+  haptic_ctx->on_press([&]() {
+    Serial.println("pressed");
+  });
+  haptic_ctx->on_release([&](unsigned long duration) {
+    print_duration(duration);
+    
+    if(duration < SHORT_PRESS_MS) {
+      ++effect;
+    } else if(duration < LONG_PRESS_MS) {
+      --effect;
+    } else {
+      haptic_ctx = aux_button->release_context();
+      aux_button->set_context(menu_ctx);
+      return;
+    }
+    effect %= MAX_EFFECT;
+    Serial.print("now playing effect: ");
+    Serial.println(effect);
+    drv.setWaveform(0, effect);
+    drv.setWaveform(1, 0);
+    drv.go();
   });
 }

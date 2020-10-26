7e22ea28bbe20436bd00ddd60fbe723771b04945
Andrew Hepp <drewhepp@umich.edu>
2020-10-25 23:46:24 -0400

    added recording mode

--- haptics.ino
+++ haptics.ino
@@ -3,10 +3,11 @@
 #include <Adafruit_DRV2605.h>
 
 #include "Button.h"
+//#include "Timer.h"
 #include "Tickable.h"
 
 constexpr unsigned long SHORT_PRESS_MS = 500;
-constexpr unsigned long LONG_PRESS_MS = 5000;
+constexpr unsigned long LONG_PRESS_MS = 3000;
 
 constexpr int BAUD_RATE = 115200;
 
@@ -17,14 +18,19 @@ constexpr int PIN_BUTTON_0 = 0;
 std::vector<Tickable*> components;
 
 Button *button;
-
-ButtonContext* menu_ctx;
-ButtonContext* led_ctx;
 ButtonContext* haptic_ctx;
+ButtonContext* record_ctx;
+ButtonContext* playback_ctx;
 
-constexpr int MAX_EFFECT = 123;
 Adafruit_DRV2605 drv;
-int effect = 0;
+constexpr std::size_t N_EFFECTS = 3; // Keep this updated if you add or remove an effect
+constexpr int DRV_EFFECT_LIB = 1;    // Described in datasheet, appears to changes fade in/out
+std::array<int, N_EFFECTS> effects   // Put the effects you want to cycle through here
+    {1, 10, 123};
+int effect_idx = N_EFFECTS - 1;
+
+unsigned long recording_start_time;
+std::vector<std::pair<unsigned long, unsigned long>> pulses;
 
 void setup() {
   Serial.begin(BAUD_RATE);
@@ -39,20 +45,19 @@ void setup() {
 
   // Initialize DRV
   drv.begin();
-  drv.selectLibrary(1);
+  drv.selectLibrary(DRV_EFFECT_LIB);
   drv.setMode(DRV2605_MODE_INTTRIG);
 
-  // Allocate aux button and push into list of components
+  // Allocate button and push into list of components
   button = new Button(PIN_BUTTON_0, false); // Again, active low
   components.push_back(button);
 
-  // Helper functions to initialize various button behaviors
-  init_menu_ctx();
-  init_led_ctx();
+  // Helper function to initialize button behavior
   init_haptic_ctx();
+  init_record_ctx();
 
-  // Button starts in "menu" mode
-  button->set_context(menu_ctx);
+  // Button starts in "haptic" mode
+  button->set_context(haptic_ctx);
   
   Serial.println("exiting setup");
 }
@@ -70,43 +75,6 @@ void print_duration(unsigned long duration) {
   Serial.println("ms");
 }
 
-void init_menu_ctx() {
-  Serial.println("initializing menu_ctx");
-  menu_ctx = new ButtonContext();
-  menu_ctx->on_press([&]() {
-    Serial.println("pressed");
-  });
-  menu_ctx->on_release([&](unsigned long duration) {
-    print_duration(duration);
-    if(duration > LONG_PRESS_MS) {
-      Serial.println("Switching to haptic controller");
-      menu_ctx = button->release_context();
-      button->set_context(haptic_ctx);
-    }
-  });
-}
-
-void init_led_ctx() {
-  Serial.println("initializing led_ctx");
-  led_ctx = new ButtonContext();
-  led_ctx->on_press([&]() {
-    Serial.println("pressed");
-  });
-  led_ctx->on_release([&](unsigned long duration) {
-    print_duration(duration);
-    
-    if(duration < SHORT_PRESS_MS) {
-      digitalWrite(PIN_LED_0, HIGH);
-    } else if(duration < LONG_PRESS_MS) {
-      digitalWrite(PIN_LED_0, LOW);
-    } else {
-      Serial.println("Switching to menu");
-      led_ctx = button->release_context();
-      button->set_context(menu_ctx);
-    }
-  });
-}
-
 void init_haptic_ctx() {
   Serial.println("initializing haptic_ctx");
   haptic_ctx = new ButtonContext();
@@ -117,22 +85,51 @@ void init_haptic_ctx() {
     print_duration(duration);
     
     if(duration < SHORT_PRESS_MS) {
-      ++effect;
-    } else if(duration < LONG_PRESS_MS) {
-      --effect;
+      // On short button press, increment effect
+      ++effect_idx;
+      effect_idx %= effects.size();
+      Serial.print("now playing idx ");
+      Serial.print(effect_idx);
+      Serial.print(", effect ");
+      Serial.println(effects[effect_idx]);
+      drv.setWaveform(0, effects[effect_idx]);
+      drv.setWaveform(1, 0);
+      drv.go();
     } else {
-      Serial.println("switching to LED controller");
+      // On long button press, start recording for 5 seconds, then switch to playback
       haptic_ctx = button->release_context();
-      button->set_context(led_ctx);
-      return;
+      button->set_context(record_ctx);
+      pulses.clear();
+      recording_start_time = millis();
+      //timer->in(
+      //  RECORDING_DURATION_MS,
+      //  [&](){
+      //    record_ctx = button->release_context();
+      //    button->set_context(playback_ctx);
+      //});
     }
-    if(effect < 0) { effect = 0; }
-    if(effect > MAX_EFFECT) { effect = MAX_EFFECT; }
-    
-    Serial.print("now playing effect: ");
-    Serial.println(effect);
-    drv.setWaveform(0, effect);
-    drv.setWaveform(1, 0);
-    drv.go();
+  });
+}
+
+void init_record_ctx() {
+  Serial.println("initializing record_ctx");
+  record_ctx = new ButtonContext();
+  record_ctx->on_press([&]() {
+    // Create pulse entry with 0 duration
+    unsigned long time_from_start = millis() - recording_start_time;
+    Serial.print("created entry at ");
+    Serial.print(time_from_start);
+    Serial.println("ms");
+    pulses.push_back(std::make_pair(time_from_start, 0));
+  });
+  record_ctx->on_release([&](unsigned long duration) {
+    // Update duration of most recent pulse
+    auto& last_entry = pulses.back();
+    last_entry.second = duration;
+    Serial.print("duration of entry at ");
+    Serial.print(last_entry.first);
+    Serial.print("ms set to ");
+    Serial.print(duration);
+    Serial.println("ms");
   });
 }
